32e2cba813a5faa368c9a7c1c9b9fec6cfa11ca1
Meiyan Xie <meiyan@blupig.net>
2017-04-21 01:31:34 -0400

    slight modify

--- angellist.py
+++ angellist.py
@@ -5,11 +5,20 @@ import urllib.parse
 url = 'https://angel.co/company_filters/search_data'
 headers = {'X-CSRF-Token': '2F3FB5ers08QkMtYXkrYpUqYnHJU+Z9MHfVFFOGuadY=', 'cookie': '_angellist=eca52b21687fd60390592a1a72f2fff4'}
 
+# just get first three pages
 for i in range(1, 4):
     body = {'filter_data[company_types][]': 'Startup', 'filter_data[stage][]': 'Seed', 'filter_data[stage][]': 'Series A', 'filter_data[stage][]': 'Series B', 'filter_data[stage][]': 'Series C', 'sort': 'Total Raised', 'page': i}
 
+    # r1 is the response of the first request
     r1 = requests.post(url, headers=headers, data=body)
     obj = json.loads(r1.text)
+    print(obj)
+
+    '''
+    from the first request,
+    we will get 20 company ids and other parameters.
+    the response is a json
+    '''
 
     # Generate url
     gen_url = ''
@@ -32,10 +41,8 @@ for i in range(1, 4):
     print(gen_url)
 
     r2 = requests.get(gen_url)
-    print(r2)
+    print(r2.content)
+    
 
-    # print(obj['ids'])
 
-# print(r.url)
-# print(r.status_code, r.reason)
-# print(r.json())
+
